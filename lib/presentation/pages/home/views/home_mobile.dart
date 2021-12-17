af9c8ba24468f7445686bc11b66ace61ede22bce
pedro pimont <pedro.pimontf@gmail.com>
2021-12-17 19:07:08 -0300

    add: small CUCO animation to home page

--- lib/presentation/pages/home/views/home_mobile.dart
+++ lib/presentation/pages/home/views/home_mobile.dart
@@ -1,7 +1,38 @@
 part of '../home.dart';
 
-class _HomeMobile extends StatelessWidget {
+class _HomeMobile extends StatefulWidget {
   const _HomeMobile({Key? key}) : super(key: key);
+
+  @override
+  State<_HomeMobile> createState() => _HomeMobileState();
+}
+
+class _HomeMobileState extends State<_HomeMobile> {
+  Point? position;
+  double size = 72;
+
+  @override
+  void initState() {
+    super.initState();
+
+    WidgetsBinding.instance?.addPostFrameCallback((_) {
+      setState(() {
+        position = Point(
+          MediaQuery.of(context).size.width / 2,
+          MediaQuery.of(context).size.height / 2,
+        );
+      });
+    });
+  }
+
+  void onCucoClick() {
+    setState(() {
+      position = Point(
+        (MediaQuery.of(context).size.width - 80) * Random().nextDouble(),
+        (MediaQuery.of(context).size.height - 160) * Random().nextDouble(),
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,7 +40,27 @@ class _HomeMobile extends StatelessWidget {
       drawer: const AppDrawer(),
       appBar: AppBar(),
       body: Stack(
-        children: [],
+        children: [
+          AnimatedPositioned(
+            duration: const Duration(milliseconds: 500),
+            left: position?.x.toDouble(),
+            bottom: position?.y.toDouble(),
+            child: GestureDetector(
+              onTap: onCucoClick,
+              child: Column(children: [
+                Image.asset(
+                  'assets/images/cuco_logo.png',
+                  width: size,
+                ),
+                const SizedBox(height: 8),
+                Text(
+                  'Click Me!',
+                  style: Theme.of(context).textTheme.headline6,
+                )
+              ]),
+            ),
+          ),
+        ],
       ),
     );
   }
